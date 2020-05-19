d7b38da146d2a6b74153bfa5d70d0c7a8582d1d0
crazypushok0 <crazypushok0@gmail.com>
2020-05-19 17:35:53 +0300

    modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-
+	$('select[name="lang"]').change(function(){
+		var lang = this.value;
+		if(lang != window.location.pathname){
+			window.location.href = lang;
+		} 
+		
+		console.log(lang);
+		
+	});
+	//Sliders
 	$('.gallery__inner').slick({
 		arrows: true,
 		dots: true,
@@ -13,5 +22,33 @@ $(document).ready(function() {
 		arrows: false,
 		fade: true,
 		asNavFor: '.reviews__items',
+	});
+	$('.work__fade-slider').slick({
+		arrows: false,
+		fade: true,
+	});
+
+
+
+	//radio_btn-color
+	$.each($('.work__fade-radio'),function(index, val){
+		if($(this).find('input').prop('checked')==true){
+			$(this).addclass('active');
+		}
+	});
+	$(document).on('click', '.work__fade-radio', function(e){
+		$(this).parents('.work__fade-color-list').find('.work__fade-radio').removeClass('active');
+		$(this).parents('.work__fade-color-list').find('.work__fade-radio input').prop('checked', false);
+		$(this).toggleClass('active');
+		$(this).find('input').prop('checked', true);
+		return false;
+
+	});
+
+
+
+	$('.arrow__top').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
 	});
 })
